b98abc3eff3f28ae5502937d09398b912f1c0a78
MordRot <mord.rot@gmail.com>
2016-11-23 18:38:53 +0200

    .

--- app/src/main/java/com/android/mor_arye/android5777_8159_8300/Model/Entities/Business.java
+++ app/src/main/java/com/android/mor_arye/android5777_8159_8300/Model/Entities/Business.java
@@ -6,6 +6,6 @@ package com.android.mor_arye.android5777_8159_8300.Model.Entities;
 
 public class Business {
     public Business() {
-        System.out.println("hello ");
+        System.out.println("hello");
     }
 }
