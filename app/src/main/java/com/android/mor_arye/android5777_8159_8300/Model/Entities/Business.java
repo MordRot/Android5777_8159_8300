5f6390636ef87f31cb94db871af851220dd80dc0
MordRot <mord.rot@gmail.com>
2016-11-23 19:41:19 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/android/mor_arye/android5777_8159_8300/Model/Entities/Business.java

--- app/src/main/java/com/android/mor_arye/android5777_8159_8300/Model/Entities/Business.java
+++ app/src/main/java/com/android/mor_arye/android5777_8159_8300/Model/Entities/Business.java
@@ -1,12 +1,19 @@
 package com.android.mor_arye.android5777_8159_8300.Model.Entities;
 
+import android.location.Address;
+import android.provider.ContactsContract;
+
 /**
  * Created by mor on 20 נובמבר 2016.
  */
 
 public class Business {
-    public Business() {
-        System.out.println("hello ");
-        System.out.println("hello 3");
-    }
+    private int idBusiness;
+    private String nameBusiness;
+    private Address addressBusiness;
+    private ContactsContract.CommonDataKinds.Phone phoneNumber;
+    private ContactsContract.CommonDataKinds.Email emailAddress;
+    private ContactsContract.CommonDataKinds.Website websiteLink;
+
+
 }
