5bee092b0fadc5ff9a0ea9936562166981076e79
MordRot <mord.rot@gmail.com>
2016-12-18 18:37:55 +0200

    עשיתי את המחלקה RegistrationActivity עוד לא עשיתי מעבר מסכים עם Intent

--- app/src/main/java/com/android/mor_arye/android5777_8159_8300/Controller/RegistrationActivity.java
+++ app/src/main/java/com/android/mor_arye/android5777_8159_8300/Controller/RegistrationActivity.java
@@ -1,14 +1,62 @@
 package com.android.mor_arye.android5777_8159_8300.Controller;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import com.android.mor_arye.android5777_8159_8300.R;
+
+import static com.android.mor_arye.android5777_8159_8300.Controller.LoginActivity.NAME_KEY;
+import static com.android.mor_arye.android5777_8159_8300.Controller.LoginActivity.PASSWORD_KEY;
 
 public class RegistrationActivity extends AppCompatActivity {
 
-    @Override
+    SharedPreferences shPrefRegister;
+    SharedPreferences.Editor editRegiser;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+    }
+
+    public void onRegister(View view) {
+
+        saveToShPref();
+        saveToDB();
+    }
+
+
+    private void saveToShPref() {
+        editRegiser = shPrefRegister.edit();
+
+        String name = ((TextView) findViewById(R.id.NameEdit)).getText().toString();
+        String pass = ((TextView) findViewById(R.id.passwordEdit)).getText().toString();
+
+        editRegiser.putString(NAME_KEY, name);
+        editRegiser.putString(PASSWORD_KEY, pass);
+        editRegiser.commit();
+    }
+
+    private void saveToDB() {
+        final ContentValues newUser = new ContentValues();
+
+        newUser.put("nameUser", ((TextView) findViewById(R.id.NameEdit)).getText().toString());
+        newUser.put("password", ((TextView) findViewById(R.id.passwordEdit)).getText().toString());
+
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... params) {
+
+                getContentResolver().insert(
+                        Uri.parse("content://com.android.mor_arye.android5777_8159_8300/users"), newUser);
+
+                return null;
+            }
+        }.execute();
     }
 }
