e1928d54d88b090fe5dc6af3a2391ebfec5c6faa
Arye <leonid1990@gmail.com>
2016-12-29 03:44:34 +0200

    Created the BR module named br_app, dependency problem

--- app/src/main/java/com/android/mor_arye/android5777_8159_8300/Model/Service/checkForUpdateService.java
+++ app/src/main/java/com/android/mor_arye/android5777_8159_8300/Model/Service/checkForUpdateService.java
@@ -2,11 +2,9 @@ package com.android.mor_arye.android5777_8159_8300.Model.Service;
 
 import android.app.IntentService;
 import android.content.Intent;
-import android.os.IBinder;
 import android.util.Log;
 import android.widget.Toast;
 
-import com.android.mor_arye.android5777_8159_8300.Model.Backend.CustomContentProvider;
 import com.android.mor_arye.android5777_8159_8300.Model.Backend.IDSManager;
 import com.android.mor_arye.android5777_8159_8300.Model.Backend.ManagerFactory;
 
@@ -24,26 +22,19 @@ public class CheckForUpdateService extends IntentService {
     }
     @Override
     protected void onHandleIntent(Intent checkForUpdatesIntent){
-        Intent businessUpdateIntent;
-        Intent userUpdateIntent;
-        businessUpdateIntent = new Intent();
-        businessUpdateIntent.setAction("UpdateDS");
-        businessUpdateIntent.addCategory("business");
-
-        userUpdateIntent = new Intent();
-        userUpdateIntent.setAction("UpdateDS");
-        userUpdateIntent.addCategory("business");
+        Intent broadcastIntent = new Intent();
 
         while (true) {
             try {
                 Thread.sleep(10000);
 
                 if (DSManager.checkNewInBusiness()) {
-                    sendBroadcast(businessUpdateIntent);
+
+                    sendBroadcast(broadcastIntent);
                 }
 
                 if (DSManager.checkNewRecreation()) {
-                    sendBroadcast(userUpdateIntent);
+                    sendBroadcast(broadcastIntent);
                 }
 
             } catch (InterruptedException e) {
